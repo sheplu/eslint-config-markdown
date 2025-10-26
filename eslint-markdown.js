60caec8b2acca4c4ac3acb4d75d56df069f0ff46
Jean Burellier <jean.burellier@gmail.com>
2025-10-26 15:12:48 +0100

    feat: import markdown rules

---
+++ eslint-markdown.js
@@ -0,0 +1,95 @@
+export const eslintMarkdownRules = {
+	rules: {
+		'markdown/fenced-code-language': [
+			'error',
+			{
+				required: [],
+			},
+		],
+		'markdown/heading-increment': [
+			'error',
+			{
+				frontmatterTitle: '',
+			},
+		],
+		'markdown/no-bare-urls': [ 'error' ],
+		'markdown/no-duplicate-definitions': [
+			'error',
+			{
+				allowDefinitions: [ '//' ],
+				allowFootnoteDefinitions: [],
+			},
+		],
+		'markdown/no-duplicate-headings': [
+			'error',
+			{
+				checkSiblingsOnly: false,
+			},
+		],
+		'markdown/no-empty-definitions': [
+			'error',
+			{
+				allowDefinitions: [ '//' ],
+				allowFootnoteDefinitions: [ 'note' ],
+				checkFootnoteDefinitions: true,
+			},
+		],
+		'markdown/no-empty-images': [ 'error' ],
+		'markdown/no-empty-links': [ 'error' ],
+		'markdown/no-html': [
+			'error',
+			{
+				allowed: [],
+				allowedIgnoreCase: false,
+			},
+		],
+		'markdown/no-invalid-label-refs': [ 'error' ],
+		'markdown/no-missing-atx-heading-space': [
+			'error',
+			{
+				checkClosedHeadings: true,
+			},
+		],
+		'markdown/no-missing-label-refs': [
+			'error',
+			{
+				allowLabels: [],
+			},
+		],
+		'markdown/no-missing-link-fragments': [
+			'error',
+			{
+				allowPattern: '',
+				ignoreCase: false,
+			},
+		],
+		'markdown/no-multiple-h1': [
+			'error',
+			{
+				frontmatterTitle: '',
+			},
+		],
+		'markdown/no-reference-like-urls': [ 'error' ],
+		'markdown/no-reversed-media-syntax': [ 'error' ],
+		'markdown/no-space-in-emphasis': [
+			'error',
+			{
+				checkStrikethrough: true,
+			},
+		],
+		'markdown/no-unused-definitions': [
+			'error',
+			{
+				allowDefinitions: [],
+				allowFootnoteDefinitions: [],
+			},
+		],
+		'markdown/require-alt-text': [ 'error' ],
+		'markdown/table-column-count': [
+			'error',
+			{
+				checkMissingCells: true,
+			},
+		],
+	},
+};
---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'eslint/config';
+import eslintRules from '@sheplu/eslint-config/src/eslint.js';
+import globals from 'globals';
+import js from '@eslint/js';
+import markdown from '@eslint/markdown';
+import { markdownRules } from './index.js';
+import stylistic from '@stylistic/eslint-plugin';
+import stylisticRules from '@sheplu/eslint-config/src/stylistic.js';
+
+// eslint-disable-next-line no-restricted-exports
+export default defineConfig([
+	{
+		'extends': [
+			'js/recommended',
+			eslintRules,
+			stylisticRules,
+		],
+		'files': [ '**/*.{js,mjs,cjs}' ],
+		'languageOptions': {
+			globals: globals.node,
+		},
+		// eslint-disable-next-line object-shorthand
+		'plugins': { '@stylistic': stylistic, js },
+		'rules': {
+		},
+	},
+	{
+		'extends': [ markdownRules ],
+		'files': [ '**/*.md' ],
+		'language': 'markdown/gfm',
+		'plugins': { markdown },
+	},
+]);
